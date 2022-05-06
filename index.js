33715b155583db7f965fea01f8e5dab564b5a256
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2022-05-06 20:56:54 +0600

    Fetch products and single product api added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
@@ -14,7 +14,35 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 app.get("/", (req, res) => {
     res.send("Inventory server is running");
 });
-
+app.get("/item/:id", (req, res) => {
+    async function run() {
+        try {
+            await client.connect();
+            const products = client.db("inventory").collection("products");
+            const query = { _id: ObjectId(req.params.id) };
+            const result = await products.findOne(query);
+            res.send(result);
+        } finally {
+            await client.close();
+        }
+    }
+    run().catch(console.dir);
+});
+app.get("/items", (req, res) => {
+    async function run() {
+        try {
+            await client.connect();
+            const products = client.db("inventory").collection("products");
+            const query = {};
+            const cursor = await products.find(query);
+            const productsResult = await cursor.toArray();
+            res.send(productsResult);
+        } finally {
+            await client.close();
+        }
+    }
+    run().catch(console.dir);
+});
 app.put("/additem", async (req, res) => {
     async function run() {
         try {
