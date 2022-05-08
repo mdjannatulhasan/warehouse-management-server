d27a81a7fc31030e9d9bfe21b38b21b98590dc96
Shah Muhammad Jannatul Hasan <58700965+mdjannatulhasan@users.noreply.github.com>
2022-05-08 23:41:22 +0600

    auth checked

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
-    console.log(authHeader);
     if (!authHeader) {
         return res.status(401).send({ message: "Unauthorised Access" });
     }
